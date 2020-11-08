dc9c42e42335870651c20ad2dd17d6879081ffd0
Irving Suárez <irving.dev@icloud.com>
2020-11-07 20:01:41 -0600

    Adding navbar

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,11 @@
 export default function() {
   return (
-    <main>
-      
-    </main>
+      <div className="container-fluid home-page">
+        <div className="row m-0">
+          <div className="col-12 text-center">
+            <h1>Hello, how are you?</h1>
+          </div>
+        </div>
+      </div>
   );
 }
