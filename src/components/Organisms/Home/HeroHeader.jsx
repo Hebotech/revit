14d233e5d0127839d8e2c4ab7622cbee6b5f8c7e
Irving Suárez <irving.dev@icloud.com>
2020-11-26 17:38:54 -0600

    Some proves

--- src/components/Organisms/Home/HeroHeader.jsx
+++ src/components/Organisms/Home/HeroHeader.jsx
@@ -4,7 +4,7 @@ export default function HeroHeader() {
       <div className="col-6 p-5 _information text-md-left">
         <small className="__feature">Performance</small>
         <h1 className="__title">
-          Superior acceleration for everyone
+          Superior acceleration 
         </h1>
         <p className="__description">
           Lorem ipsum dolor sit amet consectetur adipiscing elit bibendum magna, vestibulum cursus turpis ac gravida ut nisl neque, habitasse porttitor praesent tellus curae ligula nam arcu.
