34cdc15ae8d8d9981c1fef83e9456b02c39f969b
Irving Suárez <irving.dev@icloud.com>
2020-12-03 20:06:08 -0600

    Fixing: Deleting pre

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,13 +3,9 @@ import Statement from "../components/Organisms/Home/Statement";
 import Cta from "../components/Organisms/Home/Cta";
 import BannerSection from "../components/Organisms/Home/BannerSection";
 
-export default function index({products}) {
+export default function index() {
   return (
     <div className="container-fluid home-page p-0">
-      <pre>
-        {JSON.stringify(products, null, 2)}
-      </pre>
-
       <HeroHeader />
       
       <Statement />
@@ -19,18 +15,4 @@ export default function index({products}) {
       <Cta/>
     </div>
   );
-}
-
-export const getStaticProps = async () => {
-  console.log("Getting static props:")
-  let response = await fetch('https://apihebo.online/revit')
-  let {data: products} = await response.json()
-  console.log(products)
-
-
-  return {
-    props:{
-      products
-    }
-  }
 }
